f854e6b1d238abb763e45b02c77b4aa67833ef0e
CarlotaCiruelos <78039998+CarlotaCiruelos@users.noreply.github.com>
2022-04-27 20:46:34 +0200

    schema

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,35 @@
+DROP TABLE IF EXISTS USERS;
+
+create table USER (
+    id integer identity primary key,
+    firstName VARCHAR(45) NOT NULL,
+    lastName VARCHAR(45) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    EMAIL VARCHAR(100),
+    USER_TYPE VARCHAR(45)
+);
+
+create table ACCOUNT (
+    id integer identity primary key,
+    IBAN VARCHAR(45) NOT NULL,
+    balance float NOT NULL,
+    USER_TYPE VARCHAR(45)
+);
+
+create table ACCOUNT_USER (
+                         account_id integer identity primary key,
+                         user_id VARCHAR(45) NOT NULL
+);
+
+create table TRANSACTION (
+                              id integer identity primary key,
+                              origin_acc integer NOT NULL,
+                              destination_acc integer NOT NULL
+);
+
+create table FEES (
+                             id integer identity primary key,
+                             origin_acc integer NOT NULL,
+                             destination_acc integer NOT NULL,
+                            amount float NOT NULL
+);
