551eab1edc339de005308e3af77c00b3c39fe7b8
joserra404 <201800035@alu.comillas.edu>
2022-05-21 18:30:27 +0200

    Schema en mayusculas y otros

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,11 @@
 DROP TABLE IF EXISTS "USERS";
 
 create table "USER" (
-    "id" integer identity NOT NULL primary key,
-    "dni" VARCHAR(10) NOT NULL,
-    "firstName" VARCHAR(45) NOT NULL,
-    "lastName" VARCHAR(45) NOT NULL,
-    "password" VARCHAR(255) NOT NULL,
+    "ID" integer identity NOT NULL primary key,
+    "DNI" VARCHAR(10) NOT NULL,
+    "FIRSTNAME" VARCHAR(45) NOT NULL,
+    "LASTNAME" VARCHAR(45) NOT NULL,
+    "PASSWORD" VARCHAR(255) NOT NULL,
     "EMAIL" VARCHAR(100),
     "USER_TYPE" VARCHAR(45)
 );
@@ -13,16 +13,16 @@ create table "USER" (
 DROP TABLE IF EXISTS "ACCOUNT";
 
 create table "ACCOUNT" (
-    "id" integer identity NOT NULL primary key,
+    "ID" integer identity NOT NULL primary key,
     "IBAN" VARCHAR(45) NOT NULL,
-    "balance" float NOT NULL,
+    "BALANCE" float NOT NULL,
     "USER_TYPE" VARCHAR(45)
 );
 
 DROP TABLE IF EXISTS "ACCOUNT_USER";
 
 create table "ACCOUNT_USER" (
-                         "id" integer identity primary key,
+                         "ID" integer identity primary key,
                          "A_ID" number NOT NULL,
                          "U_ID" number NOT NULL
 );
@@ -30,19 +30,19 @@ create table "ACCOUNT_USER" (
 DROP TABLE IF EXISTS "TRANSACTION";
 
 create table "TRANSACTION" (
-                              "id" integer identity primary key,
-                              "origin_acc" integer NOT NULL,
-                              "destination_acc" integer NOT NULL,
-                              "amount" integer NOT NULL,
+                              "ID" integer identity primary key,
+                              "ORIGIN_ACC" integer NOT NULL,
+                              "DESTINATION_ACC" integer NOT NULL,
+                              "AMOUNT" integer NOT NULL,
                               "DATE" DATE NOT NULL
 );
 
 DROP TABLE IF EXISTS "FEES";
 
 create table "FEES" (
-                             "id" integer identity primary key,
+                             "ID" integer identity primary key,
                              "USER_TYPE" VARCHAR(45) NOT NULL,
-                             "fee_amount" integer NOT NULL,
+                             "FEE_AMOUNT" integer NOT NULL,
                              "COMMENT" VARCHAR(100) NOT NULL
 
 );
