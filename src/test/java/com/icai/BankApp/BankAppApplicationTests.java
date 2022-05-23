8cbd695ae4ac0b02bfe7df5116dc2957764b9edd
CarlotaCiruelos <78039998+CarlotaCiruelos@users.noreply.github.com>
2022-05-23 19:17:09 +0200

    unitary tests User

--- src/test/java/com/icai/BankApp/BankAppApplicationTests.java
+++ src/test/java/com/icai/BankApp/BankAppApplicationTests.java
@@ -3,11 +3,11 @@ package com.icai.BankApp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class BankAppApplicationTests {
 
-	/*@Test
+	@Test
 	void contextLoads() {
-	}*/
+	}
 
 }
